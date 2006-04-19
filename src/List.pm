b8107ec3c8dda85a493d7f4e31a7d00e0a7336a0
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2006-04-19 13:55:55 +0000

    Fix: ignore case where PRIMARY KEY cannot be dropped
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-5_2-branch@3719 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/List.pm
+++ src/List.pm
@@ -10976,7 +10976,6 @@ sub probe_db {
 		## drop previous primary key
 		unless ($dbh->do("ALTER TABLE $t DROP PRIMARY KEY")) {
 		    &do_log('err', 'Could not drop PRIMARY KEY, table\'%s\'.', $t);
-		    return undef;
 		}
 		push @report, sprintf('Table %s, PRIMARY KEY dropped', $t);
 		&do_log('info', 'Table %s, PRIMARY KEY dropped', $t);
