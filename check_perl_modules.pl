f6ba3d5526c77b4844d5437dd4d4c6147694de9b
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-08-14 07:27:40 +0000

    New feature: Sympa is now able to get the Email in SubjAltName. As I was not able to find the mentioned patch to mod_ssl to get SSL_CLIENT_S_EMAIL exported, I wrote another patch based on SSL_CLIENT_CERT from "SSLOptions +ExportCertData". REAPPLY the patch that had been removed.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-5.3-branch@4541 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- check_perl_modules.pl
+++ check_perl_modules.pl
@@ -87,7 +87,8 @@ use CPAN;
 	     'Net::SSLeay' => 'NET-SSLeay',
 	     'Bundle::LWP' => 'LWP',
 	     'SOAP::Lite' => 'SOAP-Lite',
-	     'File::NFSLock' => 'File-NFSLock');
+	     'File::NFSLock' => 'File-NFSLock',
+	     'Crypt::OpenSSL::X509' => 'Crypt-OpenSSL-X509');
 
 %opt_features = ('DBI' => 'a generic Database Driver, required by Sympa to access Subscriber information and User preferences. An additional Database Driver is required for each database type you wish to connect to.',
 		 'DBD::mysql' => 'Mysql database driver, required if you connect to a Mysql database.\nYou first need to install the Mysql server and have it started before installing the Perl DBD module.',
@@ -105,7 +106,8 @@ use CPAN;
 		 'Net::SSLeay' => 'required by the \'include_remote_sympa_list\' feature that includes members of a list on a remote server, using X509 authentication',
 		 'Bundle::LWP' => 'required by the \'include_remote_sympa_list\' feature that includes members of a list on a remote server, using X509 authentication',
 		 'SOAP::Lite' => 'required if you want to run the Sympa SOAP server that provides ML services via a "web service"',
-		 'File::NFSLock' => 'required to perform NFS lock ; see also lock_method sympa.conf parameter'
+		 'File::NFSLock' => 'required to perform NFS lock ; see also lock_method sympa.conf parameter',
+		 'Crypt::OpenSSL::X509' => 'required for HTTP x509 authentication (when using Email in SubjAltName)'
 		 );
 
 ### main:
