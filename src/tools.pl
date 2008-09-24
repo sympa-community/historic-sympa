72bfc156e58a85d5a323f93a9112370640b0fb20
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2008-09-24 15:52:34 +0000

    [-change] removed dead code
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-altemail@5145 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/tools.pl
+++ src/tools.pl
@@ -654,32 +654,6 @@ sub get_templates_list {
 }
 
 
-## Obtains a list of all templates at the level of a list
-sub get_list_templates_list {
-
-    my $type = shift;
-    my $robot = shift;
-    my $list = shift;
-    my $listdir;
-    my @try;
-    
-    if (defined $list) {
-	$listdir = $list->{'dir'}.'/'.$type.'_tt2';	
-	push @try, $listdir ;
-    }
-    
-    my $dirname = &get_template_path('mail',$robot,'list','','',$list);
-    unless (opendir DIR, $dirname) {
-	&do_log('err', 'ERROR in opendir');
-	return undef;
-    }
-    my @tmpdir =(sort grep(/^[^\.].+\.tt2$/,readdir DIR)); 
-    closedir DIR;
-    return @tmpdir;
-    
-}
-
-
 # return the path for a specific template
 sub get_template_path {
 
