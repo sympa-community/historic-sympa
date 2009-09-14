708b18a9128db95751e0a05aecb049d4897ff85f
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2009-09-14 09:52:46 +0000

    [bug] http_host was not set by default in syma.conf, making the sympa wizard to fail starting.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.0-branch@6199 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/confdef.pm
+++ src/lib/confdef.pm
@@ -193,6 +193,7 @@ our @params = (
         name    => 'http_host',
         default => 'http://domain.tld',
         query   => 'URL to a virtual host.',
+	default => 'http://domain.tld',
 	vhost   => '1',
         edit    => '1',
         file    => 'sympa.conf',
