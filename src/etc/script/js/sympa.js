b3566187594b0628b26d98247848f7982275dce0
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2011-05-23 12:21:59 +0000

    [dev] fixes about viewspools
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-spool@7092 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/etc/script/js/sympa.js
+++ src/etc/script/js/sympa.js
@@ -1080,11 +1080,15 @@ jQuery(document).ready(function() {
 function spoolPopup(msgkey, url, trigger, remove_if_divclass_present) {
 	if(!remove_if_divclass_present) remove_if_divclass_present = '';
 	jQuery('.viewspool').hide();
-	var p = jQuery('#viewspool' + msgkey).attr({msgkey: msgkey, ridcp: remove_if_divclass_present}).load(url, function() {
+	var p = jQuery('#viewspool' + msgkey).attr({msgkey: msgkey, ridcp: remove_if_divclass_present}).load(url, function(t) {
 		var p = jQuery(this), msgkey = p.attr('msgkey'), ridcp = p.attr('ridcp');
 		if(ridcp && p.has('.' + ridcp).length) {
-			p.parent().remove();
-			if(msgkey) jQuery('#spoolitem' + msgkey).remove();
+			p.parent().delay(2000).queue(function() {
+				jQuery(this).remove();
+			});
+			if(msgkey) jQuery('#spoolitem' + msgkey).delay(2000).queue(function() {
+				jQuery(this).remove();
+			});
 		}
 	}).parent().show();
 	if(!p.parent().is('body')) {
