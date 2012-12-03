ab28e94b2327fc23a3670c6b6c145869478f9641
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2012-12-03 15:51:00 +0000

    [dev] don't export constants, for consistency with other modules
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@8069 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Configuration.pm
+++ src/lib/Sympa/Configuration.pm
@@ -55,7 +55,7 @@ use Sympa::Template;
 use Sympa::WWSympa;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(%params %Conf DAEMON_MESSAGE DAEMON_COMMAND DAEMON_CREATION DAEMON_ALL);
+our @EXPORT = qw(%params %Conf);
 
 ## Database and SQL statement handlers
 my $sth;
