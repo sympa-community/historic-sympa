4c0de5549aefcd33196dcd32aed229e9e398a8fa
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-03-15 18:06:30 +0000

    [dev] drop dead code
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@8883 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Task.pm
+++ src/lib/Sympa/Task.pm
@@ -185,15 +185,4 @@ sub get_task_list {
     return @task_list;
 }
 
-## sort task name by their epoch date
-sub epoch_sort {
-
-    $a =~ /(\d+)\..+/;
-    my $date1 = $1;
-    $b =~ /(\d+)\..+/;
-    my $date2 = $1;
-
-    $date1 <=> $date2;
-}
-
 1;
