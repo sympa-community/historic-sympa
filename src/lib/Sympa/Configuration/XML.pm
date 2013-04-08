e979425acde1b513e5db993911ce8850d20a83fd
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-04-08 14:20:21 +0000

    [dev] fix indentation
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9064 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Configuration/XML.pm
+++ src/lib/Sympa/Configuration/XML.pm
@@ -63,18 +63,18 @@ A new L<Sympa::Configuration::XML> object, or I<undef>, if something went wrong.
 =cut
 
 sub new {
-    my ($class, %params) = @_;
-    Sympa::Log::Syslog::do_log('debug2','()');
+	my ($class, %params) = @_;
+	Sympa::Log::Syslog::do_log('debug2','()');
 
-    my $self = {};
-    my $parser = XML::LibXML->new();
-    $parser->line_numbers(1);
-    my $doc = $parser->parse_fh($params{handle});
+	my $self = {};
+	my $parser = XML::LibXML->new();
+	$parser->line_numbers(1);
+	my $doc = $parser->parse_fh($params{handle});
 
-    $self->{'root'} = $doc->documentElement();
+	$self->{'root'} = $doc->documentElement();
 
-    bless $self, $class;
-    return $self;
+	bless $self, $class;
+	return $self;
 }
 
 =back
@@ -99,39 +99,39 @@ A positive value of success, I<undef>otherwise.
 =cut
 
 sub createHash {
-    my ($self) = @_;
-    Sympa::Log::Syslog::do_log('debug2','()');
+	my ($self) = @_;
+	Sympa::Log::Syslog::do_log('debug2','()');
 
-    unless ($self->{'root'}->nodeName eq 'list') {
-	Sympa::Log::Syslog::do_log('err',"the root element must be called \"list\" ");
-	return undef;
-    }
-
-    unless (defined $self->_getRequiredElements()){
-	Sympa::Log::Syslog::do_log('err',"error in required elements ");
-	return undef;
-    }
-
-    if ($self->{'root'}->hasChildNodes()) {
-	my $hash = _getChildren($self->{'root'});
-	unless (defined $hash){
-	    Sympa::Log::Syslog::do_log('err',"error in list elements ");
-	    return undef;
+	unless ($self->{'root'}->nodeName eq 'list') {
+		Sympa::Log::Syslog::do_log('err',"the root element must be called \"list\" ");
+		return undef;
 	}
-	if (ref($hash) eq "HASH") {
-	    foreach my $k (keys %$hash) {
-		if ($k eq "type") { ## the list template creation without family context
-		    $self->{'type'} = $hash->{'type'};
-		}else {
-		    $self->{'config'}{$k} = $hash->{$k};
+
+	unless (defined $self->_getRequiredElements()){
+		Sympa::Log::Syslog::do_log('err',"error in required elements ");
+		return undef;
+	}
+
+	if ($self->{'root'}->hasChildNodes()) {
+		my $hash = _getChildren($self->{'root'});
+		unless (defined $hash){
+			Sympa::Log::Syslog::do_log('err',"error in list elements ");
+			return undef;
 		}
-	    }
-	}elsif ($hash ne "") { # a string
-	    Sympa::Log::Syslog::do_log('err','the list\'s children are not homogeneous');
-	    return undef;
+		if (ref($hash) eq "HASH") {
+			foreach my $k (keys %$hash) {
+				if ($k eq "type") { ## the list template creation without family context
+					$self->{'type'} = $hash->{'type'};
+				}else {
+					$self->{'config'}{$k} = $hash->{$k};
+				}
+			}
+		}elsif ($hash ne "") { # a string
+			Sympa::Log::Syslog::do_log('err','the list\'s children are not homogeneous');
+			return undef;
+		}
 	}
-    }
-    return 1;
+	return 1;
 }
 
 =item $xml->getHash()
@@ -149,15 +149,15 @@ An hashref.
 =cut
 
 sub getHash {
-    my ($self) = @_;
-    Sympa::Log::Syslog::do_log('debug2','()');
+	my ($self) = @_;
+	Sympa::Log::Syslog::do_log('debug2','()');
 
-    my $hash = {};
+	my $hash = {};
 
-    $hash->{'type'} = $self->{'type'} if (defined $self->{'type'}); ## the list template creation without family context
-    $hash->{'config'} = $self->{'config'};
+	$hash->{'type'} = $self->{'type'} if (defined $self->{'type'}); ## the list template creation without family context
+	$hash->{'config'} = $self->{'config'};
 
-    return $hash;
+	return $hash;
 }
 
 # $xml->_getRequiredElements()
@@ -167,14 +167,14 @@ sub getHash {
 #
 # Return value: a positive value on success, I<undef> otherwise
 sub _getRequiredElements {
-    my ($self) = @_;
-    Sympa::Log::Syslog::do_log('debug3','()');
+	my ($self) = @_;
+	Sympa::Log::Syslog::do_log('debug3','()');
 
-    # listname element is obligatory
-    unless ($self->_getRequiredSingle('listname')){
-	return undef;
-    }
-    return 1;
+	# listname element is obligatory
+	unless ($self->_getRequiredSingle('listname')){
+		return undef;
+	}
+	return 1;
 }
 
 # $xml->_getMultipleAndRequiredChild($nodeName, $childName)
@@ -183,32 +183,32 @@ sub _getRequiredElements {
 #
 # Return value: the number of node with the name $nodeName
 sub _getMultipleAndRequiredChild {
-    my ($self, $nodeName, $childName) = @_;
-    Sympa::Log::Syslog::do_log('debug3','(%s,%s)',$nodeName,$childName);
+	my ($self, $nodeName, $childName) = @_;
+	Sympa::Log::Syslog::do_log('debug3','(%s,%s)',$nodeName,$childName);
 
-    my @nodes = $self->{'root'}->getChildrenByTagName($nodeName);
+	my @nodes = $self->{'root'}->getChildrenByTagName($nodeName);
 
-    unless (defined _verify_single_nodes(\@nodes)) {
+	unless (defined _verify_single_nodes(\@nodes)) {
 	return undef;
-    }
+}
 
-    foreach my $o (@nodes) {
+foreach my $o (@nodes) {
 	my @child = $o->getChildrenByTagName($childName);
 	if ($#child < 0){
-	    Sympa::Log::Syslog::do_log('err','Element "%s" is required for element "%s", line : %s',$childName,$nodeName,$o->line_number());
-	    return undef;
+		Sympa::Log::Syslog::do_log('err','Element "%s" is required for element "%s", line : %s',$childName,$nodeName,$o->line_number());
+		return undef;
 	}
 
 	my $hash = _getChildren($o);
 	unless (defined $hash) {
-	     Sympa::Log::Syslog::do_log('err','error on _getChildren(%s) ',$o->nodeName);
-	     return undef;
-	 }
+		Sympa::Log::Syslog::do_log('err','error on _getChildren(%s) ',$o->nodeName);
+		return undef;
+	}
 
 	push @{$self->{'config'}{$nodeName}},$hash;
 	$self->{'root'}->removeChild($o);
-    }
-    return ($#nodes + 1);
+}
+return ($#nodes + 1);
 }
 
 # $xml->_getRequiredSingle($nodeName)
@@ -216,61 +216,61 @@ sub _getMultipleAndRequiredChild {
 #
 # Return value: a positive value on success, I<undef> otherwise
 sub _getRequiredSingle {
-    my ($self, $nodeName) = @_;
-    Sympa::Log::Syslog::do_log('debug3','(%s)',$nodeName);
+	my ($self, $nodeName) = @_;
+	Sympa::Log::Syslog::do_log('debug3','(%s)',$nodeName);
 
-    my @nodes = $self->{'root'}->getChildrenByTagName($nodeName);
+	my @nodes = $self->{'root'}->getChildrenByTagName($nodeName);
 
-    unless (_verify_single_nodes(\@nodes)) {
+	unless (_verify_single_nodes(\@nodes)) {
 	return undef;
-    }
+}
 
-    if ($#nodes <0) {
+if ($#nodes <0) {
 	Sympa::Log::Syslog::do_log('err','Element "%s" is required for the list ',$nodeName);
 	return undef;
-    }
+}
 
-    if ($#nodes > 0) {
+if ($#nodes > 0) {
 	my @error;
 	foreach my $i (@nodes) {
-	    push (@error,$i->line_number());
+		push (@error,$i->line_number());
 	}
 	Sympa::Log::Syslog::do_log('err','Only one element "%s" is allowed for the list, lines : %s',$nodeName,join(", ",@error));
 	return undef;
-    }
+}
 
-    my $node = shift(@nodes);
+my $node = shift(@nodes);
 
-    if ($node->getAttribute('multiple')){
+if ($node->getAttribute('multiple')){
 	Sympa::Log::Syslog::do_log('err','Attribute multiple=1 not allowed for the element "%s"',$nodeName);
 	return undef;
-    }
+}
 
-    if ($nodeName eq "type") {## the list template creation without family context
+if ($nodeName eq "type") {## the list template creation without family context
 
 	my $value = $node->textContent;
 	$value =~ s/^\s*//;
 	$value =~ s/\s*$//;
 	$self->{$nodeName} = $value;
 
-    }else {
+}else {
 	my $values = _getChildren($node);
 	unless (defined $values) {
-	     Sympa::Log::Syslog::do_log('err','error on _getChildren(%s) ',$node->nodeName);
-	     return undef;
-	 }
+		Sympa::Log::Syslog::do_log('err','error on _getChildren(%s) ',$node->nodeName);
+		return undef;
+	}
 
 	if (ref($values) eq "HASH") {
-	    foreach my $k (keys %$values) {
-		$self->{'config'}{$nodeName}{$k} = $values->{$k};
-	    }
+		foreach my $k (keys %$values) {
+			$self->{'config'}{$nodeName}{$k} = $values->{$k};
+		}
 	}else {
-	    $self->{'config'}{$nodeName} = $values;
+		$self->{'config'}{$nodeName} = $values;
 	}
-    }
+}
 
-    $self->{'root'}->removeChild($node);
-    return 1;
+$self->{'root'}->removeChild($node);
+return 1;
 }
 
 
@@ -281,98 +281,98 @@ sub _getRequiredSingle {
 # Return value: an hashref of children and their contents if elements, or a
 # string containing the value of a cdata or text section
 sub _getChildren {
-    my ($node) = @_;
-    Sympa::Log::Syslog::do_log('debug3','(%s)',$node->nodeName);
+	my ($node) = @_;
+	Sympa::Log::Syslog::do_log('debug3','(%s)',$node->nodeName);
 
-    ## return value
-    my $hash = {};
-    my $string = "";
-    my $return = "empty"; # "hash", "string", "empty"
+	## return value
+	my $hash = {};
+	my $string = "";
+	my $return = "empty"; # "hash", "string", "empty"
 
-    my $error = 0; # children not homogeneous
-    my $multiple_nodes = {};
+	my $error = 0; # children not homogeneous
+	my $multiple_nodes = {};
 
-    my @nodeList = $node->childNodes();
+	my @nodeList = $node->childNodes();
 
-    unless (_verify_single_nodes(\@nodeList)) {
+	unless (_verify_single_nodes(\@nodeList)) {
 	return undef;
-    }
+}
 
-    foreach my $child (@nodeList) {
+foreach my $child (@nodeList) {
 	my $type = $child->nodeType;
 	my $childName = $child->nodeName;
 
-        # ELEMENT_NODE
+	# ELEMENT_NODE
 	if ($type == 1) {
-	    my $values = _getChildren($child);
-	    unless (defined $values) {
-		Sympa::Log::Syslog::do_log('err','error on _getChildren(%s)',$childName);
-		return undef;
-	    }
-
-	    ## multiple
-	    if ($child->getAttribute('multiple')){
-		push @{$multiple_nodes->{$childName}},$values;
-
-	    ## single
-	    }else {
-		if (ref($values) eq "HASH") {
-		    foreach my $k (keys %$values) {
-			$hash->{$childName}{$k} = $values->{$k};
-		    }
-		}else {
-		    $hash->{$childName} = $values;
+		my $values = _getChildren($child);
+		unless (defined $values) {
+			Sympa::Log::Syslog::do_log('err','error on _getChildren(%s)',$childName);
+			return undef;
 		}
-	    }
 
-	    if ($return eq "string") {
-		$error = 1;
-	    }
-	    $return = "hash";
+		## multiple
+		if ($child->getAttribute('multiple')){
+			push @{$multiple_nodes->{$childName}},$values;
 
-	# TEXT_NODE
+			## single
+		}else {
+			if (ref($values) eq "HASH") {
+				foreach my $k (keys %$values) {
+					$hash->{$childName}{$k} = $values->{$k};
+				}
+			}else {
+				$hash->{$childName} = $values;
+			}
+		}
+
+		if ($return eq "string") {
+			$error = 1;
+		}
+		$return = "hash";
+
+		# TEXT_NODE
 	}elsif ($type == 3) {
-	    my $value = Encode::encode_utf8($child->nodeValue);
-	    $value =~ s/^\s+//;
-	    unless ($value eq "") {
-		$string = $string.$value;
+		my $value = Encode::encode_utf8($child->nodeValue);
+		$value =~ s/^\s+//;
+		unless ($value eq "") {
+			$string = $string.$value;
+			if ($return eq "hash") {
+				$error = 1;
+			}
+			$return = "string";
+		}
+
+
+		# CDATA_SECTION_NODE
+	}elsif ($type == 4) {
+		$string = $string . Encode::encode_utf8($child->nodeValue);
 		if ($return eq "hash") {
-		    $error = 1;
+			$error = 1;
 		}
 		$return = "string";
-	    }
-
-
-	# CDATA_SECTION_NODE
-	}elsif ($type == 4) {
-	    $string = $string . Encode::encode_utf8($child->nodeValue);
-	    if ($return eq "hash") {
-		$error = 1;
-	    }
-	    $return = "string";
 	}
 
 	## error
 	if ($error) {
-	    Sympa::Log::Syslog::do_log('err','(%s): the children are not homogeneous, line %s',$node->nodeName,$node->line_number());
-	    return undef;
+		Sympa::Log::Syslog::do_log('err','(%s): the children are not homogeneous, line %s',$node->nodeName,$node->line_number());
+		return undef;
 	}
-    }
+}
 
-    ## return
-    foreach my $array (keys %$multiple_nodes){
+## return
+foreach my $array (keys %$multiple_nodes){
 	$hash->{$array} = $multiple_nodes->{$array};
-    }
+}
 
-    if ($return eq "hash") {
+if ($return eq "hash") {
 	return $hash;
-    } elsif ($return eq "string") {
+} elsif ($return eq "string") {
 	$string =~ s/^\s*//;
 	$string =~ s/\s*$//;
 	return $string;
-    } else { # "empty"
+} else { # "empty"
 	return "";
-    }
+}
 }
 
 # Sympa::Configuration::XML->_verify_single_nodes($nodeList)
@@ -385,33 +385,33 @@ sub _getChildren {
 #
 # Return value: a positive value of success, I<undef> otherwise.
 sub _verify_single_nodes {
-    my ($nodeList) = @_;
-    Sympa::Log::Syslog::do_log('debug3','()');
+	my ($nodeList) = @_;
+	Sympa::Log::Syslog::do_log('debug3','()');
 
-    my $error = 0;
-    my %error_nodes;
-    my $nodeLines = _find_lines($nodeList);
+	my $error = 0;
+	my %error_nodes;
+	my $nodeLines = _find_lines($nodeList);
 
-    foreach my $node (@$nodeList) {
-	if ($node->nodeType == 1) { # ELEMENT_NODE
-	    unless ($node->getAttribute("multiple")) {
-		my $name = $node->nodeName;
-		if ($#{$nodeLines->{$name}} > 0) {
-		    $error_nodes{$name} = 1;
+	foreach my $node (@$nodeList) {
+		if ($node->nodeType == 1) { # ELEMENT_NODE
+			unless ($node->getAttribute("multiple")) {
+				my $name = $node->nodeName;
+				if ($#{$nodeLines->{$name}} > 0) {
+					$error_nodes{$name} = 1;
+				}
+			}
 		}
-	    }
 	}
-    }
-    foreach my $node (keys %error_nodes) {
-	my $lines = join ', ',@{$nodeLines->{$node}};
-	Sympa::Log::Syslog::do_log('err','Element %s is not declared in multiple but it is : lines %s',$node,$lines);
-	$error = 1;
-    }
+	foreach my $node (keys %error_nodes) {
+		my $lines = join ', ',@{$nodeLines->{$node}};
+		Sympa::Log::Syslog::do_log('err','Element %s is not declared in multiple but it is : lines %s',$node,$lines);
+		$error = 1;
+	}
 
-    if ($error) {
-	return undef;
-    }
-    return 1;
+	if ($error) {
+		return undef;
+	}
+	return 1;
 }
 
 # Sympa::Configuration::XML::->_find_lines($nodeList)
@@ -423,17 +423,17 @@ sub _verify_single_nodes {
 #
 # Return value: an hashref
 sub _find_lines {
-    my ($nodeList) = @_;
-    Sympa::Log::Syslog::do_log('debug3','()');
+	my ($nodeList) = @_;
+	Sympa::Log::Syslog::do_log('debug3','()');
 
-    my $hash = {};
+	my $hash = {};
 
-    foreach my $node (@$nodeList) {
-	if ($node->nodeType == 1){ # ELEMENT_NODE
-	    push @{$hash->{$node->nodeName}},$node->line_number();
+	foreach my $node (@$nodeList) {
+		if ($node->nodeType == 1){ # ELEMENT_NODE
+			push @{$hash->{$node->nodeName}},$node->line_number();
+		}
 	}
-    }
-    return $hash;
+	return $hash;
 }
 
 =back
