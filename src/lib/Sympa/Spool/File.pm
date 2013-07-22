1d0b5139516c6a8be869b0fcf66ba2a884aaad56
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-07-22 16:18:45 +0000

    [dev] create reference first, bless it second, for consistentcy with other classes
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9618 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Spool/File.pm
+++ src/lib/Sympa/Spool/File.pm
@@ -67,20 +67,19 @@ sub new {
 	Sympa::Log::Syslog::do_log('debug2', '(%s, %s, %s, ...)', @_);
 	my ($pkg, $spoolname, $dir, $selection_status, %opts) = @_;
 
-	my $self;
-
 	if ($selection_status and $selection_status eq 'bad') {
 		$dir .= '/bad';
 	}
 
-	$self = bless {
-		'spoolname' => $spoolname,
+	my $self = {
+		'spoolname'        => $spoolname,
 		'selection_status' => $selection_status,
-		'dir' => $dir,
-	} => $pkg;
+		'dir'              => $dir,
+	};
 	$self->{'selector'} = $opts{'selector'} if $opts{'selector'};
 	$self->{'sortby'} = $opts{'sortby'} if $opts{'sortby'};
 	$self->{'way'} = $opts{'way'} if $opts{'way'};
+	bless $self, $pkg;
 
 	Sympa::Log::Syslog::do_log('debug3', 'Spool to scan "%s"', $dir);
 
