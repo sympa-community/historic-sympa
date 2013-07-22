1668061abe73d8ae31f107f844f614c83cf6027d
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-07-22 11:25:36 +0000

    [svn] Fixing tree and textual conflicts with src/lib/Sympa/Database/MySQL.pm. Outcome: Keeps all the sympa-cleanup code and adds the AS_DOUBLE sub Soji introduced.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9576 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Database/MySQL.pm
+++ src/lib/Sympa/Database/MySQL.pm
@@ -17,8 +17,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =head1 NAME
 
@@ -301,6 +300,13 @@ sub get_indexes {
 	}
 
 	return \%indexes;
+}
+
+## For DOUBLE type.
+sub AS_DOUBLE {
+    return ( { 'mysql_type' => DBD::mysql::FIELD_TYPE_DOUBLE() } => $_[1] )
+	if scalar @_ > 1;
+    return ();
 }
 
 1;
