a07211c6b2dc91aed88019ebd8da9688c77a02c4
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-07-22 15:33:18 +0000

    [dev] enforce consistent header templates and svn properties
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9606 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/listdef.pm
+++ src/lib/Sympa/listdef.pm
@@ -1,5 +1,33 @@
-# listdef.pm - Definition of list parameters
-# RCS Identication ; $Revision$ ; $Date$
+# -*- indent-tabs-mode: t; -*-
+# vim:ft=perl:noet:sw=8:textwidth=78
+# $Id$
+#
+# Sympa - SYsteme de Multi-Postage Automatique
+# Copyrigh (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
+# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses>.
+
+=head1 NAME
+
+Sympa::listdef - Definition of list parameters
+
+=head1 DESCRIPTION
+
+This module keeps definition of list parameters.
+
+=cut
 
 package Sympa::listdef;
 
