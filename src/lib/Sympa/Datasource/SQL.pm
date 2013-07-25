a8ecff117124e3de71d1feb5b91633cd90f65896
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-07-25 13:51:14 +0000

    [svn] Resolving conflicts ont Sympa::Datasource::SQL. Outcome: All the code from Guillaume was kept; FIXM: I did not include UTF-8 usage enforcement by yet. It should probably be handled in each <RDBMS>.pl module.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9688 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Datasource/SQL.pm
+++ src/lib/Sympa/Datasource/SQL.pm
@@ -17,8 +17,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 =head1 NAME
 
@@ -238,6 +237,8 @@ A DBI statement handle object, or I<undef> if something went wrong.
 sub get_query_handle {
 	my ($self, $query) = @_;
 
+	$query =~ s/^\s+//;
+	$query =~ s/\s+$//;
 	return $self->{dbh}->prepare($query);
 }
 
