8a48aadc241bacf85aea0cf2deb64ef2fbb4e2d3
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-08-19 16:26:09 +0000

    [dev] add missing copyright statement
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9807 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Transport.pm
+++ src/lib/Sympa/Transport.pm
@@ -2,6 +2,19 @@
 # vim:ft=perl:noet:sw=8:textwidth=78
 # $Id$
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 =encoding utf-8
 
 =head1 NAME
