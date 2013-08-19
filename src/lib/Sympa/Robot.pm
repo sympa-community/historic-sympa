924e5b67586789c51730979fc36b223f66045aa8
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-08-19 16:01:21 +0000

    [dev] add missing copyright statement
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9802 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Robot.pm
+++ src/lib/Sympa/Robot.pm
@@ -2,6 +2,23 @@
 # vim:ft=perl:noet:sw=8:textwidth=78
 # $Id$
 
+# Sympa - SYsteme de Multi-Postage Automatique
+# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
+# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 =head1 NAME
 
 Sympa::Robot - Robot of mailing list service
