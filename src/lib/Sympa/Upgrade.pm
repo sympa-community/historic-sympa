1e4da0a68599174cc7e9313e91d550debd35e5c6
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-09-18 09:30:58 +0000

    [dev] enforce explicit declaration of POD charset
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-cleanup@9875 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Upgrade.pm
+++ src/lib/Sympa/Upgrade.pm
@@ -20,6 +20,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+=encoding utf-8
+
 =head1 NAME
 
 Sympa::Upgrade - Upgrade functions
