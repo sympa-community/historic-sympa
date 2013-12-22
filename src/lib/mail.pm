383ee15817868aecb84d18fd5a2d5e328bdc7d79
sikeda <sikeda@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-12-22 06:17:09 +0000

    [bug] mail::fix_part(): Newline "\n" in text/plain body should be normalized to "\r\n" if content-transfer-encoding is BASE64.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.1-branch@10029 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/mail.pm
+++ src/lib/mail.pm
@@ -1077,6 +1077,10 @@ sub fix_part($$$$) {
 	    return $part;
 	}
 
+	## normalize newline to CRLF if transfer-encoding is BASE64.
+	$newbody =~ s/\r\n|\r|\n/\r\n/g
+	    if $newenc and $newenc eq 'BASE64';
+
 	# Fix headers and body.
 	$head->mime_attr("Content-Type", "TEXT/PLAIN")
 	    unless $head->mime_attr("Content-Type");
