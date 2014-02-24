0c9c17d60783cba83a2087bec42da0799332bfaf
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2014-02-24 15:33:49 +0000

    [dev] rename Sympa::Transport package to Sympa::Transport, and ensure consistency between module and package name
    git-svn-id: https://subversion.renater.fr/sympa/trunk@10282 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Transport.pm
+++ src/lib/Sympa/Transport.pm
@@ -21,7 +21,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package SOAP::Transport::HTTP::FCGI::Sympa;
+package Sympa::Transport;
 
 use strict;
 use vars qw(@ISA);
