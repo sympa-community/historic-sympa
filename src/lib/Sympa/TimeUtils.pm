49df161b612a6b5b9756acf087a59f8b123120dd
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2014-02-24 15:46:20 +0000

    [dev] rename time_utils package to Sympa::TimeUtils
    git-svn-id: https://subversion.renater.fr/sympa/trunk@10286 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/TimeUtils.pm
+++ src/lib/Sympa/TimeUtils.pm
@@ -17,7 +17,7 @@
 ##    You should have received a copy of the GNU General Public License
 ##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##---------------------------------------------------------------------------##
-package time_utils;
+package Sympa::TimeUtils;
 
 ##---------------------------------------------------------------------------##
 ##      Date variables for date routines
