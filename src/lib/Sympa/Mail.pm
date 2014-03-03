ab3d2acee98125aca1546b9963f56d2235ff926f
rousse <rousse@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2014-03-03 15:02:13 +0000

    [dev] drop useless prototype to fix warning
    Sympa::Mail::fix_part() called too early to check prototype at src/lib/Sympa/Mail.pm line 1082.
    
    
    git-svn-id: https://subversion.renater.fr/sympa/trunk@10343 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Mail.pm
+++ src/lib/Sympa/Mail.pm
@@ -1044,7 +1044,7 @@ sub reformat_message($;$$) {
     return $msg->as_string();
 }
 
-sub fix_part($$$$) {
+sub fix_part {
     my $part        = shift;
     my $parser      = shift;
     my $attachments = shift || [];
