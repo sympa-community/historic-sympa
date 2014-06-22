bfe844488bb210cc63df1ff79922811da7803b5e
sikeda <sikeda@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2014-06-22 02:10:47 +0000

    [svn] Retrieving several modifications from sympa-6.2-branch.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@11055 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- po/sympa/check_locales.pl
+++ po/sympa/check_locales.pl
@@ -1,12 +1,13 @@
-# check_locales.pl - This script checks available locales on the system
-# Sympa uses locales to build the user interface ; if none is available,
-# Sympa will speak English only
+# -*- indent-tabs-mode: nil; -*-
+# vim:ft=perl:et:sw=4
+# $Id$
 
-# RCS Identication ; $Revision$ ; $Date$
-#
 # Sympa - SYsteme de Multi-Postage Automatique
-# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
-# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# Copyright (c) 1997, 1998, 1999 Institut Pasteur & Christophe Wolfhugel
+# Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+# 2006, 2007, 2008, 2009, 2010, 2011 Comite Reseau des Universites
+# Copyright (c) 2011, 2012, 2013, 2014 GIP RENATER
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
